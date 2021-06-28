5fba191a39732b3a994eb800aa48f4b0aa2bd700
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2021-06-28 14:37:53 +0530

    Initial Commit :heavy_check_mark:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomeContainer from '../containers/Home';
+import HomeNav from '../components/HomeNav';
 import HomeBackground from '../assets/img/homeBackground.jpg';
 
 const Index = () => {
   return (
     <HomeContainer homeBackground={HomeBackground}>
-      
+      <HomeNav />
     </HomeContainer>
   );
 }
