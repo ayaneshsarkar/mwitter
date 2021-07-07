825e750828e333e2909f23173a3fc3b9c1bf9aa5
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2021-07-07 15:24:26 +0530

    Initial Commit :heavy_check_mark:

---
+++ src/components/Posts/Main/Post.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import Avatar from '../../../assets/img/avatar.jpg';
+import Sprite from '../../../assets/svg/feather-sprite.svg';
+
+const Post = () => {
+  return (
+    <div className="posts__allPosts">
+      <div className="posts__post">
+        <div 
+          className="posts__post--icon"
+          style={{ backgroundImage: `url(${Avatar})` }}
+          >
+        </div>
+
+        <div className="posts__post--content">
+          <div className="profile">
+            <Link to="/posts" className="profile__info">
+              <h4 className="profile__info--title">Donald J. Trump 🔴⚪</h4>
+              <p className="profile__info--handle">@BayernTrump</p>
+              <p className="dot">.</p>
+              <p className="profile__info--time">5m</p>
+            </Link>
+
+            <div className="postDelete">
+              <svg className="postDelete__icon">
+                <use xlinkHref={`${Sprite}#trash`}></use>
+              </svg>
+            </div>
+          </div>
+
+          <p className="text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus accusamus possimus necessitatibus dicta soluta, veritatis deserunt ducimus sint aut perspiciatis!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CreatePost from './Main/CreatePost';
 import Main from '../../containers/Posts/Main';
+import AllPosts from './Main/Posts';
 
 const Posts = () => {
   return(
     <Main>
       <nav className="posts__main_nav">Home</nav>
       <CreatePost />
+      <AllPosts />
     </Main>
   );
 }
