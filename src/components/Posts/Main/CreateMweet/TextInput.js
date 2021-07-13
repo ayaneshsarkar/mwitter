f00125c35fb2c0ff978d967abfc62fc57ddd0f0a
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2021-07-13 16:59:01 +0530

    Initial Commit :heavy_check_mark:

--- src/components/Posts/Main/CreateMweet/TextInput.js
+++ src/components/Posts/Main/CreateMweet/TextInput.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TextInput = props => {
+  const [newLineCount, setNewLineCount] = useState(0);
+  const [textHeight, setTextHeight] = useState(90);
+
   const styleTextarea = (e) => {
     e.target.className = `createPost__form__text focused${props.textClass || ''}`;
   }
 
   const unstyleTextarea = (e) => {
-    e.target.className = `createPost__form__text${props.textClass || ''}`;
+    if(!props.value) {
+      e.target.className = `createPost__form__text${props.textClass || ''}`;
+    }
+  }
+
+  const adjustTextAreaHeight = () => {
+    if(newLineCount) {
+      setTextHeight((newLineCount * 30) + 90);
+    }
   }
 
   const setText = (e) => {
+    setTextHeight(e.target.offsetHeight);
+    setNewLineCount((e.target.value.match(/\n/g) || []).length);
+
+    adjustTextAreaHeight();
+
+    e.target.style.height = (textHeight/10) + 'rem';
     props.setValue(e.target.value);
   }
 
@@ -19,6 +36,7 @@ const TextInput = props => {
       name="post" 
       className={`createPost__form__text${props.textClass || ''}`} 
       placeholder="What's Happening?"
+
       value={props.value} 
     />
   );
