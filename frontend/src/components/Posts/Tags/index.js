5c8390a030b9efcaee1f07d7103735443042e93f
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2021-08-02 12:55:55 +0530

    Initial Commit :heavy_check_mark:

--- frontend/src/components/Posts/Tags/index.js
+++ frontend/src/components/Posts/Tags/index.js
@@ -5,15 +5,43 @@ const Tags = () => {
   return (
     <aside className="rest">
       <div className="tags">
-        <form className="tags__search">
-          <svg className="tags__search--icon">
-            <use xlinkHref={`${Sprite}#search`}></use>
-          </svg>
-          <input type="text" className="tags__search--input" placeholder="Search Mwitter" />
-        </form>
+        <div className="tags__searchBox">
+          <form className="tags__search">
+            <svg className="tags__search--icon">
+              <use xlinkHref={`${Sprite}#search`}></use>
+            </svg>
+            <input type="text" className="tags__search--input" placeholder="Search Mwitter" />
+          </form>
+        </div>
 
         <ul className="tags__list">
-          
+          <li className="tags__list--item">
+            <div className="tags__list--item-heading">What's Happening</div>
+          </li>
+
+          <li className="tags__list--item">
+            <div className="tags__list--item-link">
+              <a href="/posts">#Witcher</a>
+            </div>
+          </li>
+
+          <li className="tags__list--item">
+            <div className="tags__list--item-link">
+              <a href="/posts">#Witcher</a>
+            </div>
+          </li>
+
+          <li className="tags__list--item">
+            <div className="tags__list--item-link">
+              <a href="/posts">#Witcher</a>
+            </div>
+          </li>
+
+          <li className="tags__list--item">
+            <div className="tags__list--item-link">
+              <a href="/posts">#Witcher</a>
+            </div>
+          </li>
         </ul>
       </div>
     </aside>
