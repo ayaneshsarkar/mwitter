d7f540bcf93d1fbc162d460a99c0c8b5f0638669
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2021-08-10 23:31:25 +0530

    Initial Commit :heavy_check_mark:

--- frontend/src/components/Posts/Main/Mweet.js
+++ frontend/src/components/Posts/Main/Mweet.js
@@ -8,7 +8,7 @@ import CommentAlert from './CommentAlert';
 import Img from '../../../assets/img/SamplePic.jpg';
 import Sprite from '../../../assets/svg/feather-sprite.svg';
 
-const Mweet = ({ user, mweet, location, removePost, single }) => {
+const Mweet = ({ user, mweet, location, removePost, single, comments }) => {
   const [author, setAuthor] = useState(null);
   const [mweetAuthorUrl, setMweetAuthorUrl] = useState(null);
   const [paddingTop, setPaddingTop] = useState(0);
@@ -64,44 +64,87 @@ const Mweet = ({ user, mweet, location, removePost, single }) => {
   }
 
   return (
-    <div className={`posts__post${single ? ' single' : ''}`}>
-      {/* Profile Image */}
-      <div className="posts__post--avatar">
-        <div 
-          className="posts__post--icon"
-          style={{ 
-            backgroundImage: `url(${mweetAuthorUrl ? mweetAuthorUrl : ''})`
-          }}
-          >
+    <>
+      <div className={`posts__post${single ? ' single' : ''}`}>
+        {/* Profile Image */}
+        <div className="posts__post--avatar">
+          <div 
+            className="posts__post--icon"
+            style={{ 
+              backgroundImage: `url(${mweetAuthorUrl ? mweetAuthorUrl : ''})`
+            }}
+            >
+          </div>
         </div>
-      </div>
 
-      {/* Profile Content */}
-      <div className="posts__post--content">
-        <div className="profile">
-          <Link to="/posts" className="profile__info">
-            <h4 className="profile__info--title">
-              { author ? author.name : '' }
-            </h4>
-            <p className="profile__info--handle">
-              @{ author ? author.slug : '' }
-            </p>
-            <p className="dot singleDot">.</p>
-            <p className="profile__info--time singleTime">5m</p>
-          </Link>
+        {/* Profile Content */}
+        <div className="posts__post--content">
+          <div className="profile">
+            <Link to="/posts" className="profile__info">
+              <h4 className="profile__info--title">
+                { author ? author.name : '' }
+              </h4>
+              <p className="profile__info--handle">
+                @{ author ? author.slug : '' }
+              </p>
+              <p className="dot singleDot">.</p>
+              <p className="profile__info--time singleTime">5m</p>
+            </Link>
 
-          {(checkUser(user) && (mweet.author === user.id)) ? 
-            <div className="postDelete" onClick={() => deleteMweet(mweet.id)}>
-              <svg className="postDelete__icon">
-                <use xlinkHref={`${Sprite}#trash`}></use>
-              </svg>
-            </div> : ''
+            {(checkUser(user) && (mweet.author === user.id)) ? 
+              <div className="postDelete" onClick={() => deleteMweet(mweet.id)}>
+                <svg className="postDelete__icon">
+                  <use xlinkHref={`${Sprite}#trash`}></use>
+                </svg>
+              </div> : ''
+            }
+          </div>
+
+          {/* Mweet Content */}
+          {!single ? 
+            <>
+              <p 
+                onClick={(e) => getTag(e)}
+                className="text" dangerouslySetInnerHTML={{ __html: mweet.acf.text }}>
+              </p>
+
+              {mweet.acf.image ? 
+                <div
+                className="media"
+                onLoad={(e) => getPaddingTop(mweet, e.target.offsetWidth)}
+                style={{ 
+                  backgroundImage: `url(${mweet.acf.image.sizes.large})`, paddingTop
+                }}
+                >
+                  <img src={Img} alt="Sample" className="img" />
+                </div> 
+                : ''
+              }
+
+              {!comments ? <ul className="icons">
+                <li className="item">
+                  <svg className="icon">
+                    <use xlinkHref={`${Sprite}#heart`}></use>
+                  </svg>
+                </li>
+                <li className="item" onClick={() => setCommentBox(true)}>
+                  <svg className="icon">
+                    <use xlinkHref={`${Sprite}#message-circle`}></use>
+                  </svg>
+                </li>
+                <li className="item">
+                  <svg className="icon">
+                    <use xlinkHref={`${Sprite}#share`}></use>
+                  </svg>
+                </li>
+              </ul> : ''}
+            </>
+            : ''
           }
         </div>
 
-        {/* Mweet Content */}
-        {!single ? 
-          <>
+        {single ? 
+          <div className="posts__post--content last">
             <p 
               onClick={(e) => getTag(e)}
               className="text" dangerouslySetInnerHTML={{ __html: mweet.acf.text }}>
@@ -120,6 +163,23 @@ const Mweet = ({ user, mweet, location, removePost, single }) => {
               : ''
             }
 
+            <div className="d-flex-center postTime">
+              <p className="profile__info--time">5m</p>
+              <p className="dot">.</p>
+              <p className="profile__info--time">Aug 10, 2021</p>
+              <p className="dot">.</p>
+              <p className="profile__info--time">Mwitter For Web</p>
+            </div>
+
+            <div className="interactInfo">
+              <div className="d-flex-center item">
+                <p className="mr-1 int">500</p> Likes
+              </div>
+              <div className="d-flex-center">
+                <p className="mr-1 int">5</p> Comments
+              </div>
+            </div>
+
             <ul className="icons">
               <li className="item">
                 <svg className="icon">
@@ -137,71 +197,13 @@ const Mweet = ({ user, mweet, location, removePost, single }) => {
                 </svg>
               </li>
             </ul>
-          </>
+          </div> 
           : ''
         }
+
+        <CommentAlert open={commentBox} setClose={setCommentBox} user={user} id={mweet.id} />
       </div>
-
-      {single ? 
-        <div className="posts__post--content last">
-          <p 
-            onClick={(e) => getTag(e)}
-            className="text" dangerouslySetInnerHTML={{ __html: mweet.acf.text }}>
-          </p>
-
-          {mweet.acf.image ? 
-            <div
-            className="media"
-            onLoad={(e) => getPaddingTop(mweet, e.target.offsetWidth)}
-            style={{ 
-              backgroundImage: `url(${mweet.acf.image.sizes.large})`, paddingTop
-            }}
-            >
-              <img src={Img} alt="Sample" className="img" />
-            </div> 
-            : ''
-          }
-
-          <div className="d-flex-center postTime">
-            <p className="profile__info--time">5m</p>
-            <p className="dot">.</p>
-            <p className="profile__info--time">Aug 10, 2021</p>
-            <p className="dot">.</p>
-            <p className="profile__info--time">Mwitter For Web</p>
-          </div>
-
-          <div className="interactInfo">
-            <div className="d-flex-center item">
-              <p className="mr-1 int">500</p> Likes
-            </div>
-            <div className="d-flex-center">
-              <p className="mr-1 int">5</p> Comments
-            </div>
-          </div>
-
-          <ul className="icons">
-            <li className="item">
-              <svg className="icon">
-                <use xlinkHref={`${Sprite}#heart`}></use>
-              </svg>
-            </li>
-            <li className="item" onClick={() => setCommentBox(true)}>
-              <svg className="icon">
-                <use xlinkHref={`${Sprite}#message-circle`}></use>
-              </svg>
-            </li>
-            <li className="item">
-              <svg className="icon">
-                <use xlinkHref={`${Sprite}#share`}></use>
-              </svg>
-            </li>
-          </ul>
-        </div> 
-        : ''
-      }
-
-      <CommentAlert open={commentBox} setClose={setCommentBox} user={user} id={mweet.id} />
-    </div>
+    </>
   );
 }
 
