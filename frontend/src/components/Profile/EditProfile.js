82410f4d8f941ce7b56da25410845300588d0369
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2021-08-11 14:26:58 +0530

    Initial Commit :heavy_check_mark:

--- frontend/src/components/Profile/EditProfile.js
+++ frontend/src/components/Profile/EditProfile.js
@@ -9,6 +9,24 @@ const EditProfile = ({ open, setClose, user, status }) => {
       <div className="alert profile">
         <ProfileCover user={user} popUp={true} />
         <ProfileInfo user={user} status={status} popUp={true} />
+
+        <form className="form profileForm" encType="multipart/form-data">
+          <div className="form--box full-width">
+            <input type="text" name="name" className="form--input profile" 
+            placeholder="Full Name" value={user.name || ''} />
+            <label htmlFor="name" className="form--label profile">Full Name</label>
+          </div>
+
+          <div className="form--box full-width">
+            <textarea rows="3" type="text" name="bio" className="form--input profile" 
+            placeholder="Bio" value={''} style={{ resize: 'none' }} />
+            <label htmlFor="name" className="form--label profile">Bio</label>
+          </div>
+
+          <div className="form--box profileButton">
+            <button className="form--button">Save</button>
+          </div>
+        </form>
       </div>
     </Alert>
   )
