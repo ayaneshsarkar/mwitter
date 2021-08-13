5f840b7fcb19dc21c6fb7a13b175008deccf2b74
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2021-08-13 12:34:51 +0530

    Initial Commit :heavy_check_mark:

--- frontend/src/components/Posts/Main/MweetAlert.js
+++ frontend/src/components/Posts/Main/MweetAlert.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Alert from '../../../containers/Posts/Alert';
 import MweetBox from './CreateMweet';
 
 const MweetAlert = ({ open, setClose, user }) => {
+  const [mweetClose, setMweetClose] = useState(false);
+
+  useEffect(() => {
+    const closeAlert = () => mweetClose ? setClose(false) : null;
+    closeAlert();
+  }, [mweetClose, setClose]);
+
   return (
     <Alert open={open} setClose={setClose}>
       <div className="alert mweetBox">
         <div className="posts__main">
-          <MweetBox user={user} popUp={true} />
+          <MweetBox user={user} popUp={true} popUpClose={setMweetClose} />
         </div>
       </div>
     </Alert>
